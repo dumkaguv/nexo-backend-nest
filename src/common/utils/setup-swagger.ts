b7f5ff2ff06c0cbb6fa17ec68cf5388d3292cf82
DmitriiPC <68027431+dumkaguv@users.noreply.github.com>
2025-11-01 22:03:11 +0200

    chore: add swagger ui theme in web

--- src/common/utils/setup-swagger.ts
+++ src/common/utils/setup-swagger.ts
@@ -1,5 +1,6 @@
 import { INestApplication } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes'
 
 export function setupSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
@@ -15,10 +16,21 @@ export function setupSwagger(app: INestApplication) {
     .build()
 
   const document = SwaggerModule.createDocument(app, config)
+  const theme = new SwaggerTheme()
 
   SwaggerModule.setup('/api', app, document, {
     customSiteTitle: 'Nexo API',
     jsonDocumentUrl: '/swagger.json',
-    yamlDocumentUrl: '/swagger.yaml'
+    yamlDocumentUrl: '/swagger.yaml',
+    swaggerOptions: {
+      docExpansion: 'none',
+      persistAuthorization: true,
+      filter: true,
+      displayRequestDuration: true,
+      operationsSorter: 'alpha',
+      tagsSorter: 'alpha'
+    },
+    explorer: true,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK)
   })
 }
